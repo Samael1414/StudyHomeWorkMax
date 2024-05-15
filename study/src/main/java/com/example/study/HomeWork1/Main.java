f9afa7306716174a34bec7532b951297895d273e
Samael1414 <solaris16025@gmail.com>
2024-05-15 20:51:52 +0300

    HomeWork-15.05.2024, Fixed two method

--- study/src/main/java/com/example/study/HomeWork1/Main.java
+++ study/src/main/java/com/example/study/HomeWork1/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 //   программа должна принимать числа от пользователя и выводить их сумму.
 public class Main {
     public static void main(String[] args) {
-        cycle();
+        nonStandardStep();
     }
 
     public static void cycle() {
@@ -40,28 +40,31 @@ public class Main {
     //   Используя цикл for-each, программа должна вычислить и вывести среднее значение всех элементов массива.
     public static void averageValue() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите размер массива");
-        if (scanner.hasNextInt()) {
+        System.out.println(Messages.ENTER_ARRAY_SIZE.getMessage());
+        while (!scanner.hasNextInt()) {
+            System.out.println(Messages.getInvalidInputMessage());
+            scanner.nextLine();
+        }
             int size = scanner.nextInt();
             int[] arrayValue = new int[size];
-            System.out.println("Введите элементы массива");
+            System.out.println(Messages.ENTER_ARRAY_ELEMENTS.getMessage());
 
             for (int i = 0; i < arrayValue.length; i++) {
-                if (scanner.hasNextInt()) {
-                    arrayValue[i] = scanner.nextInt();
-                } else {
-                    System.out.println("Мне нужны цифры, а не вот это все!");
+                while (!scanner.hasNextInt()) {
+                    System.out.println(Messages.getInvalidInputMessage());
+                    scanner.nextLine();
                 }
+                arrayValue[i] = scanner.nextInt();
             }
-            int value = 0;
-            int quantity = 0;
+            double value = 0;
+            double quantity = 0;
             for (int number : arrayValue) {
                 value += number;
                 quantity++;
             }
             double average = value / quantity;
-            System.out.println("Среднее число массива: " + average);
-        }
+            System.out.println(Messages.ARRAY_AVERAGE.getMessage() + average);
+
         scanner.close();
     }
 
@@ -71,21 +74,33 @@ public class Main {
 
     public static void nonStandardStep() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите начальное значение: ");
-        if (scanner.hasNextInt()) {
-            int seedNumber = scanner.nextInt();
-            System.out.println("Введите конечное значение: ");
-            int finalNumber = scanner.nextInt();
-            System.out.println("Введите шаг: " + " ps: Только положительное число");
-            int step = scanner.nextInt();
-            if (step <= 0) {
-                System.out.println("Ты все сломал, давай по новой!");
-                return;
-            }
-            for (int i = seedNumber; i <= finalNumber; i += step) {
-                System.out.println(i);
-            }
-        } else System.out.println("Нужны только цифры и ничего иного");
+        System.out.println(Messages.ENTER_STARTING.getMessage());
+        while (!scanner.hasNextInt()) {
+            System.out.println(Messages.getInvalidInputMessage());
+            scanner.nextLine();
+        }
+        int seedNumber = scanner.nextInt();
+        System.out.println(Messages.ENTER_FINAL.getMessage());
+        while (!scanner.hasNextInt()) {
+            System.out.println(Messages.getInvalidInputMessage());
+            scanner.nextLine();
+        }
+        int finalNumber = scanner.nextInt();
+        System.out.println(Messages.ENTER_STEP.getMessage());
+        while (!scanner.hasNextInt()) {
+            System.out.println(Messages.getInvalidInputMessage());
+            scanner.nextLine();
+        }
+        int step = scanner.nextInt();
+        while (step <= 0) {
+            System.out.println(Messages.NEGATIVE_NUMBER_ERROR.getMessage());
+            scanner.nextLine();
+            step = scanner.nextInt();
+        }
+        for (int i = seedNumber; i <= finalNumber; i += step) {
+            System.out.println(i);
+        }
+
         scanner.close();
     }
 }
